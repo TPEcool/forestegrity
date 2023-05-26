2c89af7ec32902588b115473c7bd3ebed9188e4c
TPEcool <75689188+TPEcool@users.noreply.github.com>
2023-05-26 10:37:50 +0300

    Started work on broken animations when opposite keys were pressed

--- main.ts
+++ main.ts
@@ -1,32 +1,37 @@
 function AnimationEngine () {
-    if (controller.left.isPressed()) {
-        animation.runImageAnimation(
-        plr,
-        assets.animation`walkC`,
-        200,
-        true
-        )
-    } else if (controller.up.isPressed()) {
-        animation.runImageAnimation(
-        plr,
-        assets.animation`walkB`,
-        200,
-        true
-        )
-    } else if (controller.right.isPressed()) {
-        animation.runImageAnimation(
-        plr,
-        assets.animation`walkD`,
-        200,
-        true
-        )
-    } else if (controller.down.isPressed()) {
-        animation.runImageAnimation(
-        plr,
-        assets.animation`walkA`,
-        200,
-        true
-        )
+    if (controller.left.isPressed() && controller.right.isPressed() || controller.up.isPressed() && controller.down.isPressed()) {
+        animation.stopAnimation(animation.AnimationTypes.All, plr)
+        plr.setImage(assets.image`char`)
+    } else {
+        if (controller.left.isPressed()) {
+            animation.runImageAnimation(
+            plr,
+            assets.animation`walkC`,
+            200,
+            true
+            )
+        } else if (controller.up.isPressed()) {
+            animation.runImageAnimation(
+            plr,
+            assets.animation`walkB`,
+            200,
+            true
+            )
+        } else if (controller.right.isPressed()) {
+            animation.runImageAnimation(
+            plr,
+            assets.animation`walkD`,
+            200,
+            true
+            )
+        } else if (controller.down.isPressed()) {
+            animation.runImageAnimation(
+            plr,
+            assets.animation`walkA`,
+            200,
+            true
+            )
+        }
     }
 }
 function ResetAnimations () {
@@ -36,12 +41,7 @@ function ResetAnimations () {
     }
 }
 controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (true) {
-        AnimationEngine()
-    } else {
-        animation.stopAnimation(animation.AnimationTypes.All, plr)
-        plr.setImage(assets.image`char`)
-    }
+    AnimationEngine()
 })
 controller.down.onEvent(ControllerButtonEvent.Released, function () {
     ResetAnimations()
