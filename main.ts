42ed90fbd6abef3480880c66354d094e07b6909c
TPEcool <75689188+TPEcool@users.noreply.github.com>
2023-05-26 10:31:22 +0300

    Finish animation engine

--- main.ts
+++ main.ts
@@ -1,41 +1,60 @@
-function AnimationEngine (start: boolean) {
-    if (start) {
-        if (controller.left.isPressed()) {
-        	
-        } else if (controller.up.isPressed()) {
-        	
-        } else if (controller.right.isPressed()) {
-        	
-        } else if (controller.down.isPressed()) {
-        	
-        } else {
-        	
-        }
-    } else {
-    	
+function AnimationEngine () {
+    if (controller.left.isPressed()) {
+        animation.runImageAnimation(
+        plr,
+        assets.animation`walkC`,
+        200,
+        true
+        )
+    } else if (controller.up.isPressed()) {
+        animation.runImageAnimation(
+        plr,
+        assets.animation`walkB`,
+        200,
+        true
+        )
+    } else if (controller.right.isPressed()) {
+        animation.runImageAnimation(
+        plr,
+        assets.animation`walkD`,
+        200,
+        true
+        )
+    } else if (controller.down.isPressed()) {
+        animation.runImageAnimation(
+        plr,
+        assets.animation`walkA`,
+        200,
+        true
+        )
     }
 }
-controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
-	
-})
-controller.down.onEvent(ControllerButtonEvent.Released, function () {
-    resetAnim()
-})
-controller.right.onEvent(ControllerButtonEvent.Released, function () {
-    resetAnim()
-})
-controller.left.onEvent(ControllerButtonEvent.Released, function () {
-    resetAnim()
-})
-controller.up.onEvent(ControllerButtonEvent.Released, function () {
-    resetAnim()
-})
-function resetAnim () {
+function ResetAnimations () {
     if (!(controller.left.isPressed() || controller.up.isPressed() || (controller.down.isPressed() || controller.right.isPressed()))) {
         animation.stopAnimation(animation.AnimationTypes.All, plr)
         plr.setImage(assets.image`char`)
     }
 }
+controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (true) {
+        AnimationEngine()
+    } else {
+        animation.stopAnimation(animation.AnimationTypes.All, plr)
+        plr.setImage(assets.image`char`)
+    }
+})
+controller.down.onEvent(ControllerButtonEvent.Released, function () {
+    ResetAnimations()
+})
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    ResetAnimations()
+})
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
+    ResetAnimations()
+})
+controller.up.onEvent(ControllerButtonEvent.Released, function () {
+    ResetAnimations()
+})
 let plr: Sprite = null
 plr = sprites.create(assets.image`char`, SpriteKind.Player)
 tiles.setCurrentTilemap(tilemap`forest`)
